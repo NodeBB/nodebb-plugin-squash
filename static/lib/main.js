1ea1bd5517c84988b7fa6e71ae3b2a1716c162c7
psychobunny <psycho.bunny@hotmail.com>
2014-09-17 17:11:04 -0400

    reduce complexity by relying on action:plugins.superuser.setupButtons

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,53 +1,36 @@
 "use strict";
 
 (function() {
-	$(window).on('action:ajaxify.end', function(ev, data) {
-		if (data.url.match(/^user\/([\s\S]*)/)) {
-			socket.emit('plugins.superuser.isSuperUser', function(err, isSuperUser) {
-				if (isSuperUser) {
-					setupSquashButton(data);
-				}
-			});
+	var $btn;
+
+	$(window).on('action:plugins.superuser.setupButtons', function(userData) {
+		$('<a id="squash-btn" href="#" class="btn btn-primary btn-sm"></a>').insertAfter($('#unfollow-btn'));
+		$btn = $('#squash-btn');
+
+		if (userData.banned) {
+			setupSquash();
+		} else {
+			setupUnsquash();
 		}
 	});
 
-	var $btn;
-
-	function setupSquashButton(data) {
-		$.get(RELATIVE_PATH + '/api/' + data.url, function(data) {
-			if (data.isSelf) {
-				return;
-			}
-
-			$('<a id="squash-btn" href="#" class="btn btn-primary btn-sm"></a>').insertAfter($('#unfollow-btn'));
-			$btn = $('#squash-btn');
-
-			if (data.banned) {
-				setupSquash();
-			} else {
-				setupUnsquash();
-			}
+	function setupSquash() {
+		$btn.removeClass('btn-info').addClass('btn-warning').html('Squash');
+		$btn.off('click').on('click', function(ev) {
+			socket.emit('plugins.superuser.squash');
+			setupUnsquash();
+			ev.preventDefault();
+			return false;
 		});
+	}
 
-
-		function setupSquash() {
-			$btn.removeClass('btn-info').addClass('btn-warning').html('Squash');
-			$btn.off('click').on('click', function(ev) {
-				socket.emit('plugins.superuser.squash');
-				setupUnsquash();
-				ev.preventDefault();
-				return false;
-			});
-		}
-
-		function setupUnsquash() {
-			$btn.removeClass('btn-warning').addClass('btn-info').html('Unsquash');
-			$btn.off('click').on('click', function(ev) {
-				socket.emit('plugins.superuser.unsquash');
-				setupSquash();
-				ev.preventDefault();
-				return false;
-			});
-		}
+	function setupUnsquash() {
+		$btn.removeClass('btn-warning').addClass('btn-info').html('Unsquash');
+		$btn.off('click').on('click', function(ev) {
+			socket.emit('plugins.superuser.unsquash');
+			setupSquash();
+			ev.preventDefault();
+			return false;
+		});
 	}
 }());
