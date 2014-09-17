1b5219fbd87bae264849e969a17f459e46d43df9
psychobunny <psycho.bunny@hotmail.com>
2014-09-17 16:47:20 -0400

    no need for templates or styling here

--- library.js
+++ library.js
@@ -3,9 +3,6 @@
 var plugin = {};
 
 plugin.init = function(app, middleware, controllers, callback) {
-	app.get('/admin/trollify', middleware.admin.buildHeader, renderAdmin);
-	app.get('/api/admin/trollify', renderAdmin);
-
 	var SocketPlugins = module.parent.require('./socket.io/plugins');
 		SocketPlugins.superuser = SocketPlugins.superuser || {};
 		SocketPlugins.superuser.squash = squash;
@@ -20,22 +17,6 @@ function squash(socket, data, callback) {
 
 function unsquash(socket, data, callback) {
 	console.log('unsquashed');
-}
-
-
-plugin.addAdminNavigation = function(header, callback) {
-	header.plugins.push({
-		route: '/trollify',
-		icon: 'fa-tint',
-		name: 'trollify'
-	});
-
-	callback(null, header);
-};
-
-
-function renderAdmin(req, res, next) {
-	res.render('admin/trollify', {});
 }
 
 module.exports = plugin;
