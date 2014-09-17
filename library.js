84fa90ebcd55ed12f341d99ca1534f4de8a528e7
psychobunny <psycho.bunny@hotmail.com>
2014-09-17 17:27:20 -0400

    added isSuperUser check

--- library.js
+++ library.js
@@ -1,8 +1,17 @@
 "use strict";
 
-var plugin = {};
+var plugin = {},
+	winston = module.parent.require('winston'),
+	superusers;
 
 plugin.init = function(app, middleware, controllers, callback) {
+	try {
+		superusers = require('../nodebb-plugin-superusers/library');
+	} catch (e) {
+		winston.warn('Please install and activate the SuperUsers plugin in order for this to work.');
+		return callback();
+	}
+
 	var SocketPlugins = module.parent.require('./socket.io/plugins');
 		SocketPlugins.superuser = SocketPlugins.superuser || {};
 		SocketPlugins.superuser.squash = squash;
@@ -12,11 +21,25 @@ plugin.init = function(app, middleware, controllers, callback) {
 };
 
 function squash(socket, data, callback) {
-	console.log('squashed');
+	var uid = socket.uid ? socket.uid : 0;
+	superusers.isSuperUser(uid, function(err, isSuperUser) {
+		if (!isSuperUser) {
+			return callback(new Error('Not Allowed'));
+		}
+
+		console.log('squashed');
+	});
 }
 
 function unsquash(socket, data, callback) {
-	console.log('unsquashed');
+	var uid = socket.uid ? socket.uid : 0;
+	superusers.isSuperUser(uid, function(err, isSuperUser) {
+		if (!isSuperUser) {
+			return callback(new Error('Not Allowed'));
+		}
+
+		console.log('unsquashed');
+	});
 }
 
 module.exports = plugin;
